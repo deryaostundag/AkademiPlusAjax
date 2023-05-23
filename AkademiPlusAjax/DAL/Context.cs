4fbe9df078fbdd3a640a1377d9492861d11d96a5
deryaostundag <dryostundag@gmail.com>
2023-05-23 22:19:57 +0300

    Add project files.

---
+++ AkademiPlusAjax/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AkademiPlusAjax.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DERYA;initial Catalog=AkademiPlusAjaxDb;integrated Security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ AkademiPlusAjax/DAL/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace AkademiPlusAjax.DAL
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int ProductStock { get; set; }
+        public string ProductBrand { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
